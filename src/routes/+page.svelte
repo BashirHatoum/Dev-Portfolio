1021a33ebba131372f311c5967ea820af0748d97
INTALIO\bah <bashir.hatoum@intalio.com>
2025-04-17 09:33:43 +0300

    create About me section

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,7 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+    import {HeroSection, SectionHeadline} from "$components";
+
+</script>
+
+<HeroSection/>
+<SectionHeadline sectionName={"about-me"} >About Me</SectionHeadline>

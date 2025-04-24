4cfbaa6c8359951202618320127db21954f20ea0
INTALIO\bah <bashir.hatoum@intalio.com>
2025-04-24 16:07:50 +0300

    processed the sanity data to use in front-end

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -1,8 +1,11 @@
-import type { DevExperience } from '$lib/types/sanity';
-import sanityClient from '$lib/utils/sanity';
+import sanityClient, { processProjectEntries } from '$lib/utils/sanity';
 export const load = async () => {
-    const workExperience: DevExperience[]= await sanityClient.fetch('*[_type == "devExperience"] | order(startDate desc)');
+    const workExperience: SanityWorkExperience[]= await sanityClient.fetch('*[_type == "devExperience"] | order(startDate desc)');
+    const rawProjects: SanityProject[] = await sanityClient.fetch("*[_type == 'project']");
+    const projects =rawProjects.map(processProjectEntries);
+   console.log(rawProjects[0].content);
     return {
-        workExperience,
-    };
-}
+        workExperience, 
+    }
+
+} 
